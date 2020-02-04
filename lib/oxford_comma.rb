27f477ee78a781ee1bada6846f903fec79369a70
Albert LaQue <albertlaque@gmail.com>
2020-02-04 05:29:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+# strings can be operated on very similarily to arrays. you can add elements to the end of strings with <<. 
 
-end
+# METHODS AVAILABLE FOR ARRAYS TO STRINGS: 
+# .join method 
+#
+
+def oxford_comma(array)
+  if array.length > 1
+    array.insert(4, "and")
+  end
+  array.join(" ")
+end 
